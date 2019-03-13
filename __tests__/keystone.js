4e0a2a2bbe08cc26a4d313c2331e202012a3ef93
solracid <carlos.garces.garcia@gmail.com>
2019-03-13 23:22:39 +0100

    jest

--- __tests__/keystone.js
+++ __tests__/keystone.js
@@ -1,7 +1,20 @@
-let Keystone = require('keystone');
+var Keystone = require('keystone');
+
+beforeAll(()=> {
+    Keystone.init({
+        'name': 'Keystone CMS',
+        'cookie secret': 'My_Biscuit',
+      });
+    Keystone.start();
+})
 
 describe('Test Keystone CMS own features', () => {
+
     test('Keystone connects to MongoDB', () => {
         expect(Keystone.mongoose.connections._listening).toBeTruthy;
     });
 });
+
+// afterAll(()=> {
+//     Keystone.keystone.closeDatabaseConnection(()=>{})
+// })
