27482fa01639ba093cb71dbeb1f4e87ee16d6768
solracid <carlos.garces.garcia@gmail.com>
2019-04-11 20:12:35 +0200

    Mocha config

---
+++ .mocharc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    diff: true,
+    extension: ['js'],
+    opts: '',
+    //package: '../package.json',
+    //recursive: './test/mocha/',
+    reporter: 'spec',
+    slow: 75,
+    timeout: 2000,
+    ui: 'bdd'
+  };
+
+
+  //--config \"/Users/Carlos/Google Drive/Developer/recipes/test/.mocharc.js\"
--- mocha.mocharc.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    recursive: "./test/mocha/",
-};
