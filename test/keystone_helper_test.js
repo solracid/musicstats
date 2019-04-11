1cac86f1db2f1374983295339f8610fb9d940807
solracid <solracid@users.noreply.github.com>
2019-04-11 16:06:40 +0200

    Update keystone_helper_test.js

--- test/keystone_helper_test.js
+++ test/keystone_helper_test.js
@@ -1,63 +1,59 @@
 const Keystone = require('keystone');
-
-
-console.log('Before Init');
-
-Keystone.init({
-    'name': 'Keystone CMS',
-    'mongo': 'mongodb://127.0.0.1:27017/mocha_tests',
-    'cookie secret': 'My_Biscuit',
-});
-
-console.log('Keystone.init');
-
-Keystone.import('../server/models/');
-
-
-// Keystone.mongoose.connect('http://0.0.0.0:3000/', 'mocha_tests');
-// Keystone.mongoose.connection.on('open', function() {
-
-//     // Run tests here
-
-//     // Use Keystone.list('Key') to access Lists and execute queries
-//     // as you would in your main application
-
-// });
-
-Keystone.start({
-  onMount: function() {
-      console.log('KeystoneJS mounted');
-  },
-  onStart: function() {
-      console.log('KeystoneJS started');  
-  },
-  onHttpServerCreated: function() {
-      console.log('KeystoneJS HTTP');
-  },
-  onHttpsServerCreated: function() {
-      console.log('KeystoneJS HTTPS');
-  }
-});
+const assert = require('chai').assert;
 
 
 describe('Posts', function() {
+    
+    beforeAll('Start Keystone', function(done){
+        console.log('Before Init');
+
+        Keystone.init({
+            'name': 'Keystone CMS',
+            'mongo': 'mongodb://127.0.0.1:27017/mocha_tests',
+            'cookie secret': 'My_Biscuit',
+        });
+
+        console.log('Keystone.init');
+
+        Keystone.import('../server/models/');
+
+        Keystone.start({
+          onMount: function() {
+              console.log('KeystoneJS mounted');
+          },
+          onStart: function() {
+              console.log('KeystoneJS started');  
+          },
+          onHttpServerCreated: function() {
+              console.log('KeystoneJS HTTP');
+          },
+          onHttpsServerCreated: function() {
+              console.log('KeystoneJS HTTPS');
+          }
+        });
+    });
+    
+    
     beforeEach(function(done){
       if (Keystone.mongoose.connection.db) return done();
       console.log('Connecting to ' + dbURI)
       Keystone.mongoose.connect(dbURI, done);
     });
    
-    it('should be a connection to Mongo', function(done){
-      Keystone.mongoose.connection.db.should.be.a('Object');
+    it('Mongoose is connected to Mongo', function(done){
+      assert.equal(Keystone.mongoose.connection.readyState, 1);
       done();
     });
    
     it('should be a Mongoose Model', function(done) {
       Post = Keystone.list('Post');
    
-      Post.should.be.a('Object');
-      Post.should.have.property('model').be.a('Function');
-      Post.should.have.property('schema').be.a('Object');
+      assert.isObject(Post);
+      assert.Property(Post, 'model');
+      assert.isFunction(Post.model); 
+      
+      assert.Property(Post, 'schema');
+      assert.isObject(Post.schema);
    
       done();
     });
